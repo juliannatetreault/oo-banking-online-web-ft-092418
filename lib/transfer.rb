eab6f6472cec108a6b19bd340e3afcb1be9770a2
Julianna Tetreault <juliannatetreault@icloud.com>
2018-10-09 04:37:49 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,8 @@ class Transfer
   
   def execute_transactoin
     if valid? && receiver.balance > amount && self.status == "complete"
-      receiver.balance
+      receiver.balance -= amount
+      receiver.balance += amount
   end
   
   def reverse_transaction
